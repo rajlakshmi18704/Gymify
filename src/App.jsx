202d3b8d48728caa83808602da7ed82d24cd8075
Raj Lakshmi <rajlakshmi18704@gmail.com>
2024-09-25 14:26:17 +0530

    Layout of the website.
    Created the basic layout of the  website.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import {Route,Routes} from 'react-router-dom'
+import {Box} from '@mui/material'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Box width="400px" sx={{width:{xl:"14460px"}}} m="auto">
+    <Navbar/>
+ 
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/exercise/:id ' element={<ExerciseDetail/>}/>
+    </Routes>
+    <Footer/>
+   
+    </Box>
+  )
+}
+
+export default App
